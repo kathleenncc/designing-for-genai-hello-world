a6b7acaba2f734d1bfa3b0976cc76cc1308a4a2c
Kathleen Chan <kathleenxchan@gmail.com>
2026-09-21 03:29:18 -0400

    Connect app to Supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
-export default function Home() {
-  return (
-      <main>
-        <h1>Hello World</h1>
-      </main>
-  );
+import { supabase } from "@/utils/supabase/client";
+
+export default async function Home() {
+    const { data: spots, error } = await supabase
+        .from("favorite_froyo_spots")
+        .select("*")
+        .order("id");
+
+    if (error) {
+        return <main>Could not load frozen yogurt spots.</main>;
+    }
+
+    return (
+        <main>
+            <h1>My Favorite Frozen Yogurt Spots</h1>
+
+            <ul>
+                {spots.map((spot) => (
+                    <li key={spot.id}>
+                        {spot.name} — {spot.location}
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
 }
